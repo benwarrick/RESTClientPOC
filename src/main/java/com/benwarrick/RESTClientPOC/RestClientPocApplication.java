ba831674fb86e1c4f0cae862bf5d37f4b2c799c3
Benjamin Warrick <Bwarrick@petsuppliesplus.com>
2021-04-23 10:56:55 -0400

    Initial commit

---
+++ src/main/java/com/benwarrick/RESTClientPOC/RestClientPocApplication.java
@@ -0,0 +1,13 @@
+package com.benwarrick.RESTClientPOC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestClientPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestClientPocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/benwarrick/RESTClientPOC/RestClientPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.benwarrick.RESTClientPOC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestClientPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
